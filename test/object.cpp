284f76366b085f6de9ffaa1d7767de71eab2868e
Martin Ankerl <martin.ankerl@gmail.com>
2021-09-02 12:28:31 +0200

    hand written conversion of number to string
    The function appendNumToString is much faster than using a temporary
    ostringstream. The implementation is locale independent, and does not
    allocate any additional memory.
    
    In my tests, Bitcoin's BlockToJsonVerbose benchmark runs ~11% faster
    with that patch.
    
    Also added a test for the corner cases.

--- test/object.cpp
+++ test/object.cpp
@@ -7,6 +7,7 @@
 
 #include <cassert>
 #include <cstdint>
+#include <limits>
 #include <map>
 #include <memory>
 #include <stdexcept>
@@ -407,6 +408,30 @@ BOOST_AUTO_TEST_CASE(univalue_readwrite)
     BOOST_CHECK(!v.read("{} 42"));
 }
 
+BOOST_AUTO_TEST_CASE(univalue_numbers)
+{
+    UniValue v;
+    BOOST_CHECK(v.setInt(std::numeric_limits<int64_t>::max()));
+    BOOST_CHECK(v.isNum());
+    BOOST_CHECK_EQUAL(v.getValStr(), "9223372036854775807");
+
+    BOOST_CHECK(v.setInt(std::numeric_limits<int64_t>::lowest()));
+    BOOST_CHECK(v.isNum());
+    BOOST_CHECK_EQUAL(v.getValStr(), "-9223372036854775808");
+
+    BOOST_CHECK(v.setInt(int64_t(0)));
+    BOOST_CHECK(v.isNum());
+    BOOST_CHECK_EQUAL(v.getValStr(), "0");
+
+    BOOST_CHECK(v.setInt(std::numeric_limits<uint64_t>::max()));
+    BOOST_CHECK(v.isNum());
+    BOOST_CHECK_EQUAL(v.getValStr(), "18446744073709551615");
+
+    BOOST_CHECK(v.setInt(uint64_t(0)));
+    BOOST_CHECK(v.isNum());
+    BOOST_CHECK_EQUAL(v.getValStr(), "0");
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
 int main (int argc, char *argv[])
@@ -417,6 +442,7 @@ int main (int argc, char *argv[])
     univalue_array();
     univalue_object();
     univalue_readwrite();
+    univalue_numbers();
     return 0;
 }
 
